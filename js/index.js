acbb683120033ce0c25795b9a9955630bbe741ea
Meagle <mindfulyeon@gmail.com>
2020-07-13 16:49:41 +0900

    [리팩토링] html 변경

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const barElem = document.getElementById("gnb_button");
+const wrap = document.getElementById("wrap");
+console.log(barElem);
+console.log(wrap);
+// console.log("bars", bars);
+// console.log(NodeList[0]);
+
+// bars.addEventListner("click", function () {
+//   console.log("clicked");
+// });
+
+console.log("연결됨");
