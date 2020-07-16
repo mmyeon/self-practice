32f361cf078cca59a9275c693f21f741b3337c0a
Meagle <mindfulyeon@gmail.com>
2020-07-16 13:43:25 +0900

    버튼 누르면 내비게이션 메뉴 보이는 기능 추가

--- js/index.js
+++ js/index.js
@@ -1,12 +1,20 @@
-const barElem = document.getElementById("gnb_button");
-const wrap = document.getElementById("wrap");
-console.log(barElem);
-console.log(wrap);
-// console.log("bars", bars);
-// console.log(NodeList[0]);
+const gnb_button = document.getElementById("gnb_button");
+const bars = document.querySelectorAll(".bar");
+const gnb = document.querySelector(".gnb");
 
-// bars.addEventListner("click", function () {
-//   console.log("clicked");
-// });
+gnb_button.addEventListener("click", changeBar);
 
-console.log("연결됨");
+function changeBar() {
+  for (let i = 0; i < bars.length; i++) {
+    bars[i].classList.toggle("change");
+  }
+  gnb.style.display = gnb.style.display === "flex" ? "none" : "flex";
+
+  // TODO:bar의 클래스명에 change가 추가됐을때  display="flex"
+  // if (bars.classList.contains("change")) {
+  //   gnb.style.display = gnb.style.display === "none" ? "flex" : "none";
+  // }
+}
+
+// TODO:바에 이벤트를 추가하고 싶은데 데이터타입이 배열이라 element가 아니라 실패함.
+// bar에 추가하려면 배열에서 개별 요소를 가져와서 addEventListerner을 각각 먹여야되는가.
